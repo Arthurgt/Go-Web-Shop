34d04cf9083cca287a5b64338acfd1475ad572ec
Arthurgt <arturbabkiewicz@gmail.com>
2023-09-12 17:07:11 +0200

    Commit_7 - Created simple cache mechanism to store already readed files

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -2,18 +2,44 @@ package render
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"text/template"
 )
 
+var templateCache = make(map[string]*template.Template)
+
 // RenderTemplate renders templates using html
 func RenderTemplate(writter http.ResponseWriter, html string) {
-	parsedTemplate, err1 := template.ParseFiles("./templates/"+html, "./templates/base.layout.html")
-	if err1 != nil {
-		fmt.Println(err1.Error())
+	var tmpl *template.Template
+	var err error
+
+	_, inMap := templateCache[html]
+	if !inMap {
+		log.Println("Creating template and adding to cache ", html)
+		err = createTemplateCahce(html)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
 	}
-	err2 := parsedTemplate.Execute(writter, nil)
-	if err2 != nil {
-		fmt.Println(err2.Error())
+	log.Println("Using cached template ", html)
+	tmpl = templateCache[html]
+	err = tmpl.Execute(writter, nil)
+	if err != nil {
+		fmt.Println(err.Error())
 	}
+}
+
+// createTemplateCahce creates cache to store already readed files
+func createTemplateCahce(html string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", html),
+		"./templates/base.layout.html",
+	}
+	tmpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+	templateCache[html] = tmpl
+	return nil
 }
