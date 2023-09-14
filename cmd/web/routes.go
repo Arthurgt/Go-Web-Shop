22538a135d4d2996b3264789008e9164302c32b7
Arthurgt <arturbabkiewicz@gmail.com>
2023-09-14 12:11:05 +0200

    Commit_13 - Added csrf token creation

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,7 +11,8 @@ import (
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
-	mux.Use(middleware.Recoverer) 
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	return mux
--- go.mod
+++ go.mod
@@ -3,3 +3,4 @@ module webshop
 go 1.21.0
 
 require github.com/go-chi/chi v1.5.5
+require github.com/justinas/nosurf v1.1.1 // indirect
